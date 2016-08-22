ff823cf946a7902a249523fd5ad9d91a9d3032a5
OCTI-Lucas <OCTI-Lucas@OCTI>
2016-08-22 16:16:27 -0300

    Entity Usuario criada

---
+++ src/br/com/Monopoly/model/Entity/Usuario.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.Monopoly.model.Entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author samuel
+ */
+@Entity
+public class Usuario implements Serializable{
+    
+    @Id
+    private String id;
+    private String nome;
+    @Column(unique = true)
+    private String login;
+    private String senha;
+    private boolean bitDel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isBitDel() {
+        return bitDel;
+    }
+
+    public void setBitDel(boolean bitDel) {
+        this.bitDel = bitDel;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+    
+    
+    
+}
