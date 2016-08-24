c84b0428276ed42e999f5702fd2d6743a0b5a3cd
Samuel Henrique <SAMUEL>
2016-08-24 17:29:57 -0300

    Este é o commit da vitoria

--- src/br/com/Monopoly/control/Sessao.java
+++ src/br/com/Monopoly/control/Sessao.java
@@ -5,8 +5,11 @@
  */
 package br.com.Monopoly.control;
 
+import br.com.Monopoly.control.dao.PermissaoDAO;
 import br.com.Monopoly.model.Funcionalidade;
+import br.com.Monopoly.model.entity.Permissao;
 import br.com.Monopoly.model.entity.Usuario;
+import java.util.List;
 
 /**
  *
@@ -18,5 +21,20 @@ public class Sessao {
 
     public static boolean acesso(Funcionalidade funcionalidade) {
         return false;
+    }
+
+    public static boolean verificarPermissao(Funcionalidade func) {
+        if (usuario != null) {
+            Permissao permissao = new PermissaoDAO().buscarPorID(new Permissao.PermissaoID(func, usuario));
+
+            if (permissao != null) {
+                return true;
+            } else {
+                return false;
+            }
+            
+        } else {
+            return false;
+        }
     }
 }
