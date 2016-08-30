83a3e7a46e50dd4fb403469df2195495d258c5e4
Samuel Henrique Santos Monção <samu.henr@gmail.com>
2016-08-30 15:38:13 -0300

    Problemas encontradosssss.

--- src/br/com/Monopoly/view/PainelAmigosController.java
+++ src/br/com/Monopoly/view/PainelAmigosController.java
@@ -9,10 +9,19 @@ import br.com.Monopoly.control.GerenciadorDeJanelas;
 import br.com.Monopoly.control.Sessao;
 import br.com.Monopoly.control.dao.AmigosDAO;
 import br.com.Monopoly.model.entity.Amigos;
+import br.com.Monopoly.model.entity.Usuario;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Function;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -22,6 +31,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -48,20 +58,33 @@ public class PainelAmigosController implements Initializable {
 
     private Stage me;
 
-    private static List<Amigos> meusAmigos;
+    private List<Usuario> amigos;
+
+    private List<Usuario> pendentes;
+
+    private List<Amigos> meusAmigos;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         tiAmigos = new TreeItem("Amigos");
         tiPendentes = new TreeItem("Pendencias de Amizade");
         tvAmigos.setRoot(new TreeItem("Lista de Amizades"));
-        meusAmigos = new AmigosDAO().buscarAmizades(Sessao.usuario.get());
-        
-        atualizarAmigos();
+        amigos = new ArrayList<>();
+        pendentes = new ArrayList<>();
+
+        inicializadorPainelAmigos();
 
         tvAmigos.getRoot().getChildren().add(tiAmigos);
         tvAmigos.getRoot().getChildren().add(tiPendentes);
 
+        Timeline tlAtualizaAmigos = new Timeline(new KeyFrame(Duration.seconds(2), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Atualizar();
+            }
+        }));
+        tlAtualizaAmigos.setCycleCount(Timeline.INDEFINITE);
+        tlAtualizaAmigos.play();
         btAddAmigos.setOnAction((event) -> {
             Stage tela = GerenciadorDeJanelas.abrirJanela(GerenciadorDeJanelas.carregarComponente("BuscarNovoAmigo"), "Encontre seu Amigo !!", GerenciadorDeJanelas.Tipo.UNDECORATED, GerenciadorDeJanelas.Tipo.UNRESIZABLE);
             tela.initModality(Modality.WINDOW_MODAL);
@@ -73,53 +96,64 @@ public class PainelAmigosController implements Initializable {
             @Override
             public void run() {
                 me = (Stage) apPrincipal.getScene().getWindow();
+
             }
         });
     }
-    public void limpar(){
-        
+
+    public void limpar() {
+
     }
-    
-    public void atualizarAmigos() {
+
+    public void Atualizar() {
+
+    }
+
+    public void inicializadorPainelAmigos() {
         tiAmigos.getChildren().clear();
         tiPendentes.getChildren().clear();
+        meusAmigos = new AmigosDAO().buscarAmizades(Sessao.usuario.get());
+
         for (Amigos meuAmigo : meusAmigos) {
             if (meuAmigo.isAceito()) {
                 if (meuAmigo.getId().getConvidado().equals(Sessao.usuario.get())) {
-
+                    amigos.add(meuAmigo.getId().getRemetente());
                     tiAmigos.getChildren().add(new TreeItem(meuAmigo.getId().getRemetente()));
                 } else {
                     tiAmigos.getChildren().add(new TreeItem(meuAmigo.getId().getConvidado()));
+                    amigos.add(meuAmigo.getId().getConvidado());
                 }
             } else if (meuAmigo.getId().getConvidado().equals(Sessao.usuario.get())) {
-                
+
                 HBox hbSolicitacao = new HBox();
                 TreeItem solicitacao = new TreeItem(meuAmigo.getId().getRemetente());
                 Button btAceitarSolicitacao = new Button("Aceitar");
                 btAceitarSolicitacao.setOnAction((event) -> {
                     meuAmigo.setAceito(true);
                     new AmigosDAO().editar(meuAmigo);
-                    atualizarAmigos();
+                    inicializadorPainelAmigos();
                 });
 
                 Button btRecusarSolicitacao = new Button("Recusar");
                 btRecusarSolicitacao.setOnAction((event) -> {
                     meuAmigo.setDel(true);
                     new AmigosDAO().editar(meuAmigo);
-                    atualizarAmigos();
+                    inicializadorPainelAmigos();
                 });
 
                 hbSolicitacao.getChildren().add(btAceitarSolicitacao);
                 hbSolicitacao.getChildren().add(btRecusarSolicitacao);
                 hbSolicitacao.setSpacing(20d);
                 solicitacao.setGraphic(hbSolicitacao);
+                pendentes.add(meuAmigo.getId().getRemetente());
                 tiPendentes.getChildren().add(solicitacao);
+
             } else {
+                pendentes.add(meuAmigo.getId().getConvidado());
                 tiPendentes.getChildren().add(new TreeItem(meuAmigo.getId().getConvidado()));
             }
 
         }
-
     }
 
 }
