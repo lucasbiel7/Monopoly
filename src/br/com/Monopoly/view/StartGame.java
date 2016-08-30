bde16a56c727006770d73865fc418431383303d4
Samuel Henrique Santos Monção <samu.henr@gmail.com>
2016-08-30 17:16:26 -0300

    Problemas encontradosssss.

--- src/br/com/Monopoly/view/StartGame.java
+++ src/br/com/Monopoly/view/StartGame.java
@@ -6,9 +6,6 @@
 package br.com.Monopoly.view;
 
 import br.com.Monopoly.control.GerenciadorDeJanelas;
-import br.com.Monopoly.control.Seguranca;
-import br.com.Monopoly.control.Sessao;
-import br.com.Monopoly.control.dao.UsuarioDAO;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -22,18 +19,18 @@ public class StartGame extends Application {
     public void start(Stage primaryStage) {
         //Desenvolvedor
 //        Sessao.usuario = new UsuarioDAO().login("master", "123");
-        Sessao.usuario.set(new UsuarioDAO().login("lucasbiel7", Seguranca.criptografar("lucas")));
-        if(Sessao.usuario.getValue() == null){
-            Sessao.usuario.set(new UsuarioDAO().login("s", Seguranca.criptografar("s")));
-            if(Sessao.usuario.getValue() == null){
-                Sessao.usuario.setValue(new UsuarioDAO().buscarTodos().get(0));
-            }
-        }
+//        Sessao.usuario.set(new UsuarioDAO().login("lucas2", Seguranca.criptografar("123")));
+//        if(Sessao.usuario.getValue() == null){
+//            Sessao.usuario.set(new UsuarioDAO().login("s", Seguranca.criptografar("s")));
+//            if(Sessao.usuario.getValue() == null){
+//                Sessao.usuario.setValue(new UsuarioDAO().buscarTodos().get(0));
+//            }
+//        }
         
-        GerenciadorDeJanelas.abrirJanela(GerenciadorDeJanelas.carregarComponente("Principal"), "Tela principal", GerenciadorDeJanelas.Tipo.MAXIMIZED, GerenciadorDeJanelas.Tipo.UNDECORATED).show();
+//        GerenciadorDeJanelas.abrirJanela(GerenciadorDeJanelas.carregarComponente("Principal"), "Tela principal", GerenciadorDeJanelas.Tipo.MAXIMIZED, GerenciadorDeJanelas.Tipo.UNDECORATED).show();
 //        GerenciadorDeJanelas.abrirJanela(GerenciadorDeJanelas.carregarComponente("GerenciarPerfil"), "Perfil - Monopoly Deal", GerenciadorDeJanelas.Tipo.EXIT_ON_CLOSE, GerenciadorDeJanelas.Tipo.UNRESIZABLE).show();
 //Instalador
-//        GerenciadorDeJanelas.abrirJanela(GerenciadorDeJanelas.carregarComponente("Login"), "Monopoly Deal", GerenciadorDeJanelas.Tipo.EXIT_ON_CLOSE,GerenciadorDeJanelas.Tipo.UNRESIZABLE).show();
+        GerenciadorDeJanelas.abrirJanela(GerenciadorDeJanelas.carregarComponente("Login"), "Monopoly Deal", GerenciadorDeJanelas.Tipo.EXIT_ON_CLOSE,GerenciadorDeJanelas.Tipo.UNRESIZABLE).show();
     }
 
     /**
