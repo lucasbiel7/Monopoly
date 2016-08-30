2202f04401e45a78850d822b4662c576bb1e714e
Samuel Henrique Santos Monção <samu.henr@gmail.com>
2016-08-30 16:11:26 -0300

    Problemas encontradosssss.

--- src/br/com/Monopoly/view/PainelAmigosController.java
+++ src/br/com/Monopoly/view/PainelAmigosController.java
@@ -106,7 +106,7 @@ public class PainelAmigosController implements Initializable {
     }
 
     public void Atualizar() {
-
+        inicializadorPainelAmigos();
     }
 
     public void inicializadorPainelAmigos() {
