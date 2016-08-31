693b9ed2f7998d11b6968bd4de8410588851a958
Samuel Henrique Santos Monção <samu.henr@gmail.com>
2016-08-31 17:04:28 -0300

    Tela de Configuracao
    Completa e otimizada !!!!!!!!!!

--- src/br/com/Monopoly/view/ConfiguracaoSalaController.java
+++ src/br/com/Monopoly/view/ConfiguracaoSalaController.java
@@ -5,8 +5,12 @@
  */
 package br.com.Monopoly.view;
 
+import br.com.Monopoly.control.Alerta;
+import br.com.Monopoly.control.dao.SalaDAO;
+import br.com.Monopoly.model.entity.Sala;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +20,7 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -47,20 +52,39 @@ public class ConfiguracaoSalaController implements Initializable {
 
     @FXML
     private Button btSalvar;
-    
+
+    private Stage me;
+
+    private Sala sala;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        spNumeroDeJogadores.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(2, 5, 5));
-    }    
-    
-     @FXML
-    void btCancelarEvent(ActionEvent event) {
+        Platform.runLater(() -> {
+            me = (Stage) apPrincipal.getScene().getWindow();
+            sala = (Sala) apPrincipal.getUserData();
+            spNumeroDeJogadores.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(2, 5, sala.getCapacidade()));
+        });
 
+        
+    }
+
+    @FXML
+    void btCancelarEvent(ActionEvent event) {
+        me.close();
     }
 
     @FXML
     void btSalvarEvent(ActionEvent event) {
+        if (sala != null) {
+            int quantidadeJogador = spNumeroDeJogadores.getValue();
 
+            sala.setCapacidade(quantidadeJogador);
+            new SalaDAO().editar(sala);
+            Alerta.criarAlert(Alerta.tipoAlerta.CONCLUIDO).show();
+            me.close();
+        } else {
+            System.out.println("null");
+        }
     }
 
 }
